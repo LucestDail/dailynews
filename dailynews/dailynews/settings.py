646ff84957b9a0508542e4e6ef79e2e4639e4d0b
hyun <dhtmdgussksm@naver.com>
2022-02-05 19:19:34 +0900

    omg sorry
    crontab added

--- dailynews/dailynews/settings.py
+++ dailynews/dailynews/settings.py
@@ -144,9 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CronTabs Job Field
 
 CRONJOBS = [
-
     ('* * * * *', 'dailynews.cron.scrap_every_minute', '>> cron.log'),
     ('* * * * *', 'bs4news.cron.scrap_every_minute', '>> bs4cron.log'),
     ('* * * * *', 'bs4news.cron.news_analysis_create_morphs', '>> bs4cron.log'),
-    ('* * * * *', 'main.cron.scrap_every_minute', '>> main4cron.log'),
+    ('* * * * *', 'main.cron.dashboard_every_minute', '>> main4cron.log'),
 ]
--- dailynews/main/cron.py
+++ dailynews/main/cron.py
@@ -4,19 +4,15 @@ from main.models import Dashboard
 
 
 def dashboard_every_minute():
-    # Dashboard_Total_News_Count = models.CharField(max_length=200)
-    # Dashboard_Total_Analysis_Count = models.CharField(max_length=200)
-    # Dashboard_Total_Analysis_Rate = models.CharField(max_length=200)
-    # Dashboard_Today_count = models.CharField(max_length=200)
-
     print('django main dashboard_every_minute crontab started -------------------')
     news_data_count = News.objects.all()
     news_data_count_input = len(news_data_count)
-
+    print(news_data_count_input)
     news_analysis_count_data = News_Analysis_Raw.objects.all()
     news_analysis_count = len(news_analysis_count_data)
-
+    print(news_analysis_count)
     news_analysis_rate = round((news_analysis_count / news_data_count_input) * 100)
+    print(news_analysis_rate)
     today_check_date = datetime.today() - timedelta(days=1)
     today_input_date = str(today_check_date.year) + '-' + str(today_check_date.month) + '-' + str(today_check_date.day)
     today_from_date = datetime.strptime(today_input_date, '%Y-%m-%d').date()
@@ -25,13 +21,13 @@ def dashboard_every_minute():
     today_news_data_date = News_Analysis_Raw.objects.filter(News_Analysis_CreateDT__range=(today_from_date,
                                                                                            today_to_date))
     today_news_date_count = len(today_news_data_date)
-    news_analysis_morphs = News_Analysis_Raw(
+    print(today_news_date_count)
+    Dashboard_instance = Dashboard(
         Dashboard_Total_News_Count=news_data_count_input,
         Dashboard_Total_Analysis_Count=news_analysis_count,
         Dashboard_Total_Analysis_Rate=news_analysis_rate,
         Dashboard_Today_count=today_news_date_count,
-        News_Analysis_CreateDT=datetime.now(),
+        Dashboard_CreateDT=datetime.now(),
     )
-    news_analysis_morphs.save()
-
+    Dashboard_instance.save()
     print('django main dashboard_every_minute crontab started -------------------')
