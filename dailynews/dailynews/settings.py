1f0fd26cfaec4c2a696341db953d6353b7e06f53
hyun <dhtmdgussksm@naver.com>
2022-04-22 21:48:18 +0900

    Update settings.py
    crontab time setting change

--- dailynews/dailynews/settings.py
+++ dailynews/dailynews/settings.py
@@ -144,8 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CronTabs Job Field
 
 CRONJOBS = [
-    ('20 */2 * * *', 'bs4news.cron.scrap', '>> bs4cron.log'),
-    ('40 */2 * * *', 'bs4news.cron.news_analysis_create_morphs', '>> bs4cron.log'),
-    ('0 */2 * * *', 'main.cron.dashboard_every_minute', '>> main4cron.log'),
-    ('0 */12 * * *', 'bs4news.cron.news_analysis_create_news_dashboard_data', '>> bs4cron.log'),
+    ('20 * * * *', 'bs4news.cron.scrap', '>> bs4cron.log'),
+    ('40 * * * *', 'bs4news.cron.news_analysis_create_morphs', '>> bs4cron.log'),
+    ('0 * * * *', 'main.cron.dashboard_every_minute', '>> main4cron.log'),
+    ('0 * * * *', 'bs4news.cron.news_analysis_create_news_dashboard_data', '>> bs4cron.log'),
 ]
