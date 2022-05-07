e5be8820526cd72a36db9938c988cf2304aadfd2
hyun <dhtmdgussksm@naver.com>
2022-05-07 14:15:36 +0900

    selenium crawler test
    test utility added

--- dailynews/bs4news/urls.py
+++ dailynews/bs4news/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('morphs', views.create_morphs, name='morphs'),
     path('indexmorphs', views.index_morphs, name='indexmorphs'),
     path('word2vec', views.word2vec, name='word2vec'),
-
+    path('bs4crawl', views.bs4crawl, name='bs4crawl'),
+    path('bs4scrap', views.bs4scrap, name='bs4scrap'),
 ]
--- dailynews/bs4news/views.py
+++ dailynews/bs4news/views.py
@@ -1,8 +1,10 @@
+import sys
+import traceback
 from datetime import datetime, timedelta
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from konlpy.tag import Okt
-from .models import News, News_Company, News_Analysis_Raw
+from .models import *
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize, sent_tokenize
 import time
@@ -10,15 +12,17 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import warnings
+import random
 import pandas as pd
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import urllib.request
 import zipfile
 from lxml import etree
+from pathlib import Path
 
 warnings.filterwarnings("ignore")
 # Create your views here.
-
 
 def chart(request):
     news_data_all = News.objects.all()
@@ -119,7 +123,7 @@ def scrap(request):
                                     articleTimeHourConvert = articleTimeHour
                                 articleTimeHourResult = datetime.strptime(articleTimeHourConvert.strftime("%H:%M"), '%H:%M').time()
                                 inputarticleTime = articleTimeDate.strftime('%Y-%m-%d') + ' ' + articleTimeHourResult.strftime('%H:%M:%S')
-                                time.sleep(5)
+                                time.sleep(random.uniform(0, 1.0))
                                 # News_from
                                 # News_title
                                 # News_contents
@@ -187,25 +191,31 @@ def create_morphs(request):
     # convert_minute_company = str(current_minute).zfill(3)
     # target_company = convert_minute_company
     okt = Okt()
-    from_date = current_datetime - timedelta(days=15)
+    from_date = current_datetime - timedelta(days=1)
     to_date = current_datetime
-    target_news_data = News.objects.filter(News_CreateDT__range=(from_date, to_date))
+    target_news_data = News.objects.filter(News_company=target_company, News_CreateDT__range=(from_date, to_date))
     print('target data count===')
     print(len(target_news_data))
     target = 0
     success = 0
     fail = 0
     for target_news_element in target_news_data:
+        excluded_word_data = BS4_NEWS_ANALYSIS_WORD_EXCLUDED.objects.filter(COMPANY_CODE=target_news_element.News_company)
+        excluded_word_list = []
+        if len(excluded_word_data) > 0:
+            for excluded_word_data_element in excluded_word_data:
+                excluded_word_list.append(excluded_word_data_element.EXCLUDED_WORD)
         target_news_morphs = okt.nouns(target_news_element.News_contents)
         target_news_morphs = [n for n in target_news_morphs if len(n) > 1]
         save_morphs = ''
         count = 0
         target += 1
         for morphs_element in target_news_morphs:
-            save_morphs += morphs_element
-            count += 1
-            if count < len(target_news_morphs):
-                save_morphs += ","
+            if morphs_element not in excluded_word_list:
+                save_morphs += morphs_element
+                count += 1
+                if count < len(target_news_morphs):
+                    save_morphs += ","
         if (News_Analysis_Raw.objects.filter(
                 News_Analysis_From=target_news_element.News_from,
                 News_Analysis_Title=target_news_element.News_title,
@@ -320,3 +330,208 @@ def word2vec(request):
         print('news for ended ================================')
     print('word2vce ended -------------------')
     return render(request, 'bs4news.html', {'news_list': target_news_sentences})
+
+
+def bs4scrap(request):
+    if request.method == 'GET' and 'company' in request.GET and 'url' in request.GET:
+        url = request.GET['url']
+        company_code = request.GET['company']
+    else:
+        url = 'www.khan.co.kr/newest/articles'
+        company_code = '032'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            for ulElements in soup.find_all("ul", class_="type02"):
+                for href in ulElements.find_all("li"):
+                    print(href.find("a")["href"])
+                    visiturl = href.find("a")["href"]
+                    try:
+                        visitResponse = requests.get(visiturl, headers=headers)
+                        if response.status_code == 200:
+                            try:
+                                visitHtml = visitResponse.text
+                                visitSoup = BeautifulSoup(visitHtml, 'html.parser')
+                                articleTitle = visitSoup.find(class_='media_end_head_headline').text.strip()
+                                articleBody = visitSoup.find(id='dic_area').text.strip()
+                                articleCompany = url[url.find('oid')+4:url.find('oid')+7]
+                                if type(visitSoup.find(class_='byline')) is None:
+                                    articleBy = '기자 정보 없음'
+                                else:
+                                    articleBy = visitSoup.find(class_='byline').text.strip()
+                                articleTime = visitSoup.find(class_='media_end_head_info_datestamp_time').text.strip()
+                                articleTimeDate = datetime.strptime(articleTime.split()[0],'%Y.%m.%d.')
+                                articleTimeAP = articleTime.split()[1]
+                                articleTimeHour = datetime.strptime(articleTime.split()[2], '%H:%M')
+                                if(articleTimeAP == '오후'):
+                                    articleTimeHourConvert = timedelta(hours=12) + articleTimeHour
+                                else:
+                                    articleTimeHourConvert = articleTimeHour
+                                articleTimeHourResult = datetime.strptime(articleTimeHourConvert.strftime("%H:%M"), '%H:%M').time()
+                                inputarticleTime = articleTimeDate.strftime('%Y-%m-%d') + ' ' + articleTimeHourResult.strftime('%H:%M:%S')
+                                time.sleep(5)
+                                # News_from
+                                # News_title
+                                # News_contents
+                                # News_CreateDT
+                                if(News.objects.filter(
+                                    News_from=articleBy,
+                                    News_title=articleTitle
+                                )):
+                                    print('duplicated, next article')
+                                else:
+                                    news_instance = News(
+                                        News_from=articleBy,
+                                        News_title=articleTitle,
+                                        News_contents=articleBody,
+                                        News_CreateDT=inputarticleTime,
+                                        News_company=articleCompany
+                                    )
+                                    news_instance.save()
+                                    print('save success')
+                            except Exception:
+                                pass
+                        else:
+                            print(visitResponse.status_code)
+                            print("end work")
+                    except Exception:
+                        pass
+        else:
+            print(response.status_code)
+            print("end work")
+    except Exception:
+        pass
+
+
+def bs4crawl(request):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                      "AppleWebKit/537.36 (KHTML, like Gecko)"
+                      "Chrome/87.0.4280.88 Safari/537.36"
+    }
+    browser = webdriver.Chrome(str(Path(__file__).resolve().parent.parent) + '/webdriver/chromedriver_mac')
+    browser.implicitly_wait(time_to_wait=5)
+    bs4_news_company_list = BS4_NEWS_COMPANY.objects.all()
+    input_date = str(datetime.now().year) + '-'\
+                 + str(datetime.now().month) + '-'\
+                 + str(datetime.now().day) + " "\
+                 + str(datetime.now().hour) + ":"\
+                 + str(datetime.now().minute) + ":"\
+                 + str(datetime.now().second)
+    from_date = datetime.strptime(input_date, '%Y-%m-%d %H:%M:%S')
+    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> JOB START")
+    for bs4_news_company_element in bs4_news_company_list:
+        target_company_code = bs4_news_company_element.COMPANY_CODE
+        bs4_news_company_crawl_list = BS4_NEWS_COMPANY_CRAWL.objects.filter(COMPANY_CODE=target_company_code)
+        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> current target company : " + target_company_code)
+        for bs4_news_company_crawl_element in bs4_news_company_crawl_list:
+            target_url = bs4_news_company_crawl_element.DOMAIN_URL + bs4_news_company_crawl_element.DOMAIN_QUERY_STRING
+            browser.get(target_url)
+            bs4_news_company_crawl_link_xpath = bs4_news_company_crawl_element.CRAWL_LINK_TARGET
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> current target url : " + target_url)
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> search target : "
+                  + bs4_news_company_crawl_link_xpath)
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> request proceeding...")
+            try:
+                target_list_group = browser.find_element(by=By.XPATH,
+                                                         value=bs4_news_company_crawl_element.CRAWL_LINK_TARGET)
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> accessing list collect start")
+                element_link_set = {find_target_element.get_attribute('href')
+                                for find_target_element
+                                in target_list_group.find_elements_by_tag_name("a")}
+                crawl_count = len(element_link_set)
+                crawl_success = 0
+                crawl_fail = 0
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> accessing list count : " + str(len(element_link_set)))
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> accessing list collect end")
+                for element_link_set_element in element_link_set:
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> current access target_url : "
+                          + element_link_set_element)
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> request proceeding...")
+                    try:
+                        response = requests.get(element_link_set_element, headers=headers)
+                        if response.status_code == 200:
+                            html = response.text
+                            if (BS4_NEWS_COMPANY_CRAWL_DATA.objects.filter(
+                                    COMPANY_CODE=target_company_code,
+                                    TARGET_CRAWL_URL=element_link_set_element
+                            )):
+                                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> duplicated, next article")
+                            else:
+                                BS4_NEWS_COMPANY_CRAWL_DATA_INPUT = BS4_NEWS_COMPANY_CRAWL_DATA(
+                                    COMPANY_CODE=target_company_code,
+                                    DOMAIN_URL=bs4_news_company_crawl_element.DOMAIN_URL,
+                                    DOMAIN_QUERY_STRING=bs4_news_company_crawl_element.DOMAIN_QUERY_STRING,
+                                    TARGET_CRAWL_URL=element_link_set_element,
+                                    TARGET_RAW_HTML_DATA=html,
+                                    UPDATE_DATETIME=datetime.now(),
+                                    UPDATE_USER='SYSTEM',
+                                    CREATE_DATETIME=datetime.now(),
+                                    CREATE_USER='SYSTEM',
+                                    DESCRIPTION_INFO='NONE',
+                                )
+                                crawl_success += 1
+                                BS4_NEWS_COMPANY_CRAWL_DATA_INPUT.save()
+                                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> save success")
+                        else:
+                            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> response Error Code : "
+                                  + response.status_code)
+                        time.sleep(random.uniform(0, 1.0))
+                    except Exception:
+                        BS4_NEWS_COMPANY_CRAWL_DATA_INPUT = BS4_NEWS_COMPANY_CRAWL_DATA(
+                            COMPANY_CODE=target_company_code,
+                            DOMAIN_URL=bs4_news_company_crawl_element.DOMAIN_URL,
+                            DOMAIN_QUERY_STRING=bs4_news_company_crawl_element.DOMAIN_QUERY_STRING,
+                            TARGET_CRAWL_URL=element_link_set_element,
+                            TARGET_RAW_HTML_DATA=str(traceback.format_exc()),
+                            UPDATE_DATETIME=datetime.now(),
+                            UPDATE_USER='SYSTEM_ERROR',
+                            CREATE_DATETIME=datetime.now(),
+                            CREATE_USER='SYSTEM_ERROR',
+                            DESCRIPTION_INFO='NONE',
+                        )
+                        crawl_fail += 1
+                        BS4_NEWS_COMPANY_CRAWL_DATA_INPUT.save()
+                        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> response Exception")
+                        pass
+
+                BS4_NEWS_COMPANY_CRAWL_RESULT_INPUT = BS4_NEWS_COMPANY_CRAWL_RESULT(
+                    COMPANY_CODE=target_company_code,
+                    DOMAIN_URL=bs4_news_company_crawl_element.DOMAIN_URL,
+                    DOMAIN_QUERY_STRING=bs4_news_company_crawl_element.DOMAIN_QUERY_STRING,
+                    EXECUTE_DATE=datetime.now(),
+                    EXECUTE_RESULT_COUNT=str(crawl_count),
+                    EXECUTE_RESULT_FAIL=str(crawl_fail),
+                    EXECUTE_RESULT_SUCCESS=str(crawl_success),
+                    EXECUTE_RESULT='END',
+                    UPDATE_DATETIME=datetime.now(),
+                    UPDATE_USER='SYSTEM',
+                    CREATE_DATETIME=datetime.now(),
+                    CREATE_USER='SYSTEM',
+                    DESCRIPTION_INFO='NONE',
+                )
+                BS4_NEWS_COMPANY_CRAWL_RESULT_INPUT.save()
+
+            except Exception:
+                print(traceback.format_exc())
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> request Exception")
+                pass
+        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> close target company : " + target_company_code)
+    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " >> JOB END")
+    browser.close()
+    input_date = str(datetime.now().year) + '-' \
+                 + str(datetime.now().month) + '-' \
+                 + str(datetime.now().day) + " " \
+                 + str(datetime.now().hour) + ":" \
+                 + str(datetime.now().minute) + ":" \
+                 + str(datetime.now().second)
+    to_date = datetime.strptime(input_date, '%Y-%m-%d %H:%M:%S')
+    # news_list = {}
+    news_list = BS4_NEWS_COMPANY_CRAWL_DATA.objects.filter(
+        CREATE_DATETIME__range=(from_date, to_date),
+    )
+    return render(request, 'bs4newst.html', {'news_list': news_list})
